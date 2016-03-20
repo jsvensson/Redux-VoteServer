6810ac0984f33046c6f8645e3772268aaeeb7e53
Johan Svensson <johan@atomicplayboy.net>
2016-03-20 23:57:34 +0100

    Basic implementation of next()

--- src/core.js
+++ src/core.js
@@ -13,3 +13,11 @@ export function next(state) {
 
   return nextState
 }
+
+export function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  )
+}
