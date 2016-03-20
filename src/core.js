333e10c12973fa8f2834470420a816cc5c21c633
Johan Svensson <johan@atomicplayboy.net>
2016-03-20 23:33:09 +0100

    Implemented next()

--- src/core.js
+++ src/core.js
@@ -1,5 +1,15 @@
-import {List} from 'immutable'
+import {List, Map} from 'immutable'
 
 export function setEntries(state, entries) {
   return state.set('entries', List(entries))
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+  const nextState =  state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  })
+
+  return nextState
 }
