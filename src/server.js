c99961b38b9cc9e77cfdaef109ac5ecfb8a20a40
Johan Svensson <johan@atomicplayboy.net>
2016-03-21 11:56:12 +0100

    Server events

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 import Server from 'socket.io'
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090)
+
+  // Subscribe to state requests
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  )
+
+  io.on('connection', (socket) => {
+    // Emit state on new connection
+    socket.emit('state', store.getState().toJS())
+
+    // Listen for incoming actions
+    socket.on('action', store.dispatch.bind(store))
+  })
 }
